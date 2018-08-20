b9ccef53d8dc648adb4771ffe5a013cfe8c6f45b
RachaelNantale <rachaelnantale42@gmail.com>
2018-08-20 16:24:07 +0300

    [Finishes #159757463]Remove unnecesary space

--- app/models/answer_models.py
+++ app/models/answer_models.py
@@ -3,17 +3,17 @@ import uuid
 
 
 class AnswersModels:
-    def __init__(self, body):
-        self.body = body
-        self.time = datetime.datetime.now()
+    def __init__(self, answer):
         self.id = uuid.uuid4().hex
+        self.answer = answer
+        self.time = datetime.datetime.now()
 
     def to_answerjson(self):
         """
         json representation of the answers model
         """
-        return {'id': self.body,
-                'title': self.time,
-                'body': self.id
-
-                }
+        return {
+            'id': self.id,
+            'answer': self.answer,
+            'time': self.time
+        }
