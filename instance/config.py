ee77d5275565d2704b29df8ce6e471f9c0c64374
RachaelNantale <rachaelnantale42@gmail.com>
2018-08-20 07:01:13 +0300

    [#159791935] Add configurations folder

---
+++ instance/config.py
@@ -0,0 +1,24 @@
+import os
+
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    SECRET = 'secr3tk3y'
+
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Configurations for Testing."""
+    TESTING = True
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
