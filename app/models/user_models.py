f8e9fde6b848602cf2100c2cc097ad72a71ebbfc
RachaelNantale <rachaelnantale42@gmail.com>
2018-08-23 16:59:56 +0300

    [#159914386 #159914381]Implement SQL Queries in the user models

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -1,5 +1,8 @@
 import uuid
-import re
+from flask_bcrypt import Bcrypt
+from DBHandler import MyDatabase
+
+db = MyDatabase()
 
 
 class UserModel():
@@ -8,3 +11,15 @@ class UserModel():
         self.User_ID = uuid.uuid4().hex
         self.email = email.strip(" ")
         self.password = password.strip(" ")
+
+    def create_user(self):
+        sql = "INSERT INTO UserTable values('{}','{}','{}')RETURNING User_ID".format(
+            self.User_ID, self.email, self.password)
+        db.create_login(sql)
+
+    def fetch_user(self):
+        sql = "SELECT * FROM UserTable WHERE email = '{}'".format(
+            self.email)
+        db.fetch_user(sql)
+
+    
