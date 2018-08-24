9afe9d26b16f3e98c5ff25d1878a7693ffa83f20
RachaelNantale <rachaelnantale42@gmail.com>
2018-08-24 11:09:14 +0300

    Refactor code

--- app/utilities/utilities.py
+++ app/utilities/utilities.py
@@ -14,24 +14,24 @@ def validate_user_input(email, password):
     return UserModel(email=email, password=password)
 
 
-def validate_question_input(title, body, tags, Question_ID):
+def validate_question_input(title, description, tags):
     """ Function to validate data entered while creating a question """
     if len(title) < 5 or title == "" or title == " ":
         return {'message': 'Title is too short. Please add more clarity'}, 400
     if re.compile('[!@#$%^&*:;?><.]').match(title):
         return {'message': 'Please dont input symbols'}, 400
 
-    if len(body) < 5 or body == " ":
+    if len(description) < 5 or description == " ":
         return {'message': 'Description is too short.'}, 400
-    if re.compile('[!@#$%^&*:;?><.]').match(body):
+    if re.compile('[!@#$%^&*:;?><.]').match(description):
         return {'message': 'Please dont input symbols'}, 400
 
     if not tags or tags == "":
         return {'message': 'Please a correct tag'}, 400
     if re.compile('[!@#$%^&*:;?><.]').match(tags):
         return {'message': 'Please dont input symbols'}, 400
-    return QuestionsModels(title=title, body=body,
-                           tags=tags, Question_ID=Question_ID)
+    return QuestionsModels(title=title, description=description,
+                           tags=tags)
 
 
 def validate_answer_input(answer):
