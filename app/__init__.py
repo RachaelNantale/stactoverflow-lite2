dd301477910c47d0c65087648f44702d8b3f53bc
RachaelNantale <rachaelnantale42@gmail.com>
2018-08-26 06:13:50 +0300

    [Feature #160042902]Add JWT configurations

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.views.views import bp
 from app.views.answer_views import answer_bp
 from app.views.user_views import user_bp
+from flask_bcrypt import Bcrypt
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token, get_jwt_identity)
 
@@ -10,7 +11,8 @@ def create_app(config_name):
     """Method for creating the app"""
     app = Flask(__name__)
     app.config['JWT_SECRET_KEY'] = 'sup3rsecr3t'
-    JWTManager(app)
+    jwt = JWTManager(app)
+    bcrypt = Bcrypt(app)
 
     app.config.from_object(config_name)
     app.register_blueprint(bp)
