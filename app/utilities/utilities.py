a1b4cbccd15d65974e087a750547aae40f3992b1
RachaelNantale <rachaelnantale42@gmail.com>
2018-08-28 12:41:06 +0300

    [Feature #159914455]Modify the validation

--- app/utilities/utilities.py
+++ app/utilities/utilities.py
@@ -1,7 +1,4 @@
 import re
-from app.models.user_models import UserModel
-from app.models.models import QuestionsModels
-from app.models.answer_models import AnswersModels
 
 
 def validate_user_input(email, password=None):
@@ -10,8 +7,6 @@ def validate_user_input(email, password=None):
         return {'message': 'Please input a valid email'}, 400
     if len(password) < 5:
         return {'message': 'This password is not strong enough '}, 400
-
-    return UserModel(email=email, password=password)
 
 
 def validate_question_input(title, description, tags):
@@ -30,8 +25,6 @@ def validate_question_input(title, description, tags):
         return {'message': 'Please a correct tag'}, 400
     if re.compile('[!@#$%^&*:;?><.]').match(tags):
         return {'message': 'Please dont input symbols'}, 400
-    return QuestionsModels(title=title, description=description,
-                           tags=tags)
 
 
 def validate_answer_input(answer):
@@ -40,4 +33,3 @@ def validate_answer_input(answer):
         return {'message': 'Please use a valid answer input '}, 400
     if re.compile('[!@#$%^&*:;?><.0-9]').match(answer):
         return {'message': 'Please dont input symbols'}, 400
-    return AnswersModels(answer=answer)
