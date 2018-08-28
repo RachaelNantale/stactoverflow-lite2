b3d98583956a23f02864d7296303068a08d5c2e2
RachaelNantale <rachaelnantale42@gmail.com>
2018-08-28 12:39:36 +0300

    [Feature  #160081857]Add Database to the routes

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,10 @@
 import datetime
 import uuid
+import re
+
+from DBHandler import MyDatabase
+
+db = MyDatabase()
 
 
 class QuestionsModels:
@@ -15,6 +20,16 @@ class QuestionsModels:
     def get_id(self):
         return self.Question_ID
 
+    def create_question(self):
+        if validate_question_input(self.title, self.description,
+                                   self.tags):
+            sql = "INSERT INTO QuestionTable values('{}','{}','{}','{}','{}')RETURNING Question_ID".format(
+                self.Question_ID, self.title, self.description, self.tags,
+                self.time)
+            result = db.create_user(sql)
+            return result
+        return False
+
     def to_questionjson(self):
         """
         json representation of the questions model
@@ -25,3 +40,23 @@ class QuestionsModels:
             'Description': self.description,
             'tags': self.tags
         }
+
+
+def validate_question_input(title, description, tags):
+    """ Function to validate data entered while creating a question """
+    if len(title) < 5 or title == "" or title == " ":
+        return {'message': 'Title is too short. Please add more clarity'}, 400
+    if re.compile('[!@#$%^&*:;?><.]').match(title):
+        return {'message': 'Please dont input symbols'}, 400
+
+    if len(description) < 5 or description == " ":
+        return {'message': 'Description is too short.'}, 400
+    if re.compile('[!@#$%^&*:;?><.]').match(description):
+        return {'message': 'Please dont input symbols'}, 400
+
+    if not tags or tags == "":
+        return {'message': 'Please a correct tag'}, 400
+    if re.compile('[!@#$%^&*:;?><.]').match(tags):
+        return {'message': 'Please dont input symbols'}, 400
+    return QuestionsModels(title=title, description=description,
+                           tags=tags)
--- app/models/user_models.py
+++ app/models/user_models.py
@@ -1,7 +1,8 @@
 import uuid
 from flask_bcrypt import Bcrypt
 from DBHandler import MyDatabase
-
+import re
+from app.utilities.utilities import validate_user_input
 db = MyDatabase()
 
 
@@ -12,14 +13,47 @@ class UserModel():
         self.email = email.strip(" ")
         self.password = password.strip(" ")
 
+    # def create_user(self):
+    #     sql = "INSERT INTO UserTable values('{}','{}','{}')RETURNING User_ID".format(
+    #         self.User_ID, self.email, self.password)
+    #     db.create_user(sql)
+
+    # def fetch_user(self):
+    #     sql = "SELECT * FROM UserTable WHERE email = '{}'".format(
+    #         self.email)
+    #     db.fetch_user(sql)
+
+    # def check_user_exists(self):
+    #     query = "SELECT * FROM UserTable WHERE email = '{}'".format(self.email)
+    #     return db.check_user_exists(query)
+
     def create_user(self):
-        sql = "INSERT INTO UserTable values('{}','{}','{}')RETURNING User_ID".format(
+        validate = validate_user_input(self.email, self.password)
+        if validate:
+            return validate
+
+        query = "SELECT * FROM UserTable WHERE email = '{}'".format(self.email)
+
+        if db.check_user_exists(query):
+            return {'Message': 'User already exists'}
+
+        sql = "INSERT INTO UserTable values('{}','{}','{}')".format(
             self.User_ID, self.email, self.password)
-        db.create_user(sql)
+        print(sql)
+        return db.create_user(sql)
 
-    def fetch_user(self):
-        sql = "SELECT * FROM UserTable WHERE email = '{}'".format(
-            self.email)
-        db.fetch_user(sql)
+    def fetch_user(self, username):
+        if validate_user_input(self.email, self.password):
+            sql = "SELECT * FROM UserTable WHERE email = '{}'".format(
+                self.email)
+            # print(sql)
+            return db.fetch_user(sql)
+        return False
 
-    
+
+# def validate_user_input(self, email, password=None):
+
+#     if not re.match(r"([\w\.-]+)@([\w\.-]+)(\.[\w\.]+$)", email):
+#         return {'message': 'Please input a valid email'}, 400
+#     if len(password) < 5:
+#         return {'message': 'This password is not strong enough '}, 400
