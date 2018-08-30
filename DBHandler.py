882b392bced5532c0a50bad946dd8bc1c9bb4a72
RachaelNantale <rachaelnantale42@gmail.com>
2018-08-30 07:28:34 +0300

    [Feature #160081857 ]Add SQL Queries for the PUT endpoint

--- DBHandler.py
+++ DBHandler.py
@@ -39,7 +39,7 @@ class MyDatabase():
         ON DELETE CASCADE ON UPDATE CASCADE,
         answered_by varchar(50) NOT NULL,
         status bool DEFAULT False
-        
+
         ) """
         self.cur.execute(answers_table)
 
@@ -51,9 +51,7 @@ class MyDatabase():
         self.cur.execute(drop_Users_table)
         self.cur.execute(drop_questions_table)
         self.cur.execute(drop_answers_table)
-# 333333
 
-#######################################################################################################
     def create_item(self, sql):
         result = self.cur.execute(sql)
         return {'message': 'Created succesfully'}, 201
@@ -81,7 +79,8 @@ class MyDatabase():
             my_dict['title'] = question[1]
             my_dict['description'] = question[2]
             my_dict['tags'] = question[3]
-            my_dict['time'] = question[4]
+            my_dict['Asked By'] = question[4]
+            my_dict['Time'] = question[5]
 
             my_questions.append(my_dict)
         return my_questions
@@ -99,12 +98,53 @@ class MyDatabase():
                 my_dict['description'] = question[2]
                 my_dict['tags'] = question[3]
                 my_dict['time'] = question[4]
-                print(my_dict)
                 return my_dict
             return None
 
+    def fetch_single_question_by_id(self, Question_ID):
+        self.cur.execute(
+            "SELECT * FROM QuestionTable WHERE Question_ID = '{}' ".format(Question_ID))
+        question = self.cur.fetchone()
+        if question:
+            return question
+
+    def fetch_answer_by_id(self, Answer_ID):
+        self.cur.execute(
+            "SELECT * FROM AnswerTable WHERE  Answer_ID = '{}'".format(Answer_ID))
+        answer = self.cur.fetchone()
+        if answer:
+            return answer
+
+    def fetch_user_by_email(self, email):
+        self.cur.execute(
+            "SELECT * FROM Usertable WHERE email = '{}'".format(email))
+        email = self.cur.fetchone()
+        if email:
+            return email
+        self.connection.commit()
+
+    def check_answer_exists(self, Question_ID, answer):
+        self.cur.execute(
+            "SELECT  * FROM ANswerTable  WHERE Question_Id = '{}' and  answer = '{}'".format(Question_ID, answer))
+        answer = self.cur.fetchone()
+        if answer:
+            return True
+        return False
+
+    def fetch_answer_details(self, Question_ID, Answer_ID):
+        self.cur.execute(
+            "SELECT * FROM AnswerTable WHERE Question_ID = '{}' and Answer_ID = '{}'".format(Question_ID, Answer_ID))
+        details = self.cur.fetchone()
+        return details
+
+    def fetch_a_question(self, Question_ID):
+        self.cur.execute(
+            "SELECT * FROM QuestionTable WHERE Question_ID = '{}'" .format(Question_ID))
+        row = self.cur.fetchone()
+        return row
+
     def fetch_all_answers(self, Question_ID):
-        self.cur.execute("SELECT title,body,tags FROM QuestionTable WHERE Question_ID = '{}'".format(
+        self.cur.execute("SELECT * FROM QuestionTable WHERE Question_ID = '{}'".format(
             Question_ID))
         question = self.cur.fetchone()
         self.cur.execute(
@@ -118,21 +158,46 @@ class MyDatabase():
                     'Answer_ID': answers[index][0],
                     'created at': answers[index][1],
                     'answer': answers[index][2],
-                    'status': answers[index][4]
+                    'Answered by': answers[index][4],
+                    'Status': answers[index][5]
 
                 })
             my_answers.append(user_answers)
-        return {'Question': {'title': question[0],
-                             'Description': question[1],
-                             'tags': question[2],
-                             'answers': my_answers
+        return {'Question': {'title': question[1],
+                             'Description': question[2],
+                             'tags': question[3],
+                             'answers': my_answers,
+                             'Asked by': question[4]
 
                              }}
 
-    def update_answer(self, query3):
-        if self.cur.execute(query3) is None:
-            return True
-        return False
+    def update_answer(self, answer, Answer_ID, Question_ID):
+        try:
+            self.cur.execute("UPDATE AnswerTable SET answer = '{}' where Answer_ID = '{}' and Question_ID = '{}'" .format(
+                answer, Answer_ID, Question_ID))
+            cmd = self.cur.rowcount
+            if int(cmd) > 0:
+                return "Answer successfully updated"
+            else:
+                return "Answer not updated, or doesn't exist"
+
+        except Exception as exception:
+            return {"message": str(exception)}, 400
+
+    def accept_answer(self, status, Question_ID, Answer_ID):
+
+        try:
+            query = ("""UPDATE answers SET status = '{}' where Answer_ID = '{}' and Question_ID = '{}'""" .format(
+                status, Answer_ID, Question_ID))
+            self.cur.execute(query)
+            count = self.cur.rowcount
+            if int(count) > 0:
+                return "Answer successfully accepted"
+            else:
+                return "Failed to accept answer, or it doesn't exist"
+
+        except Exception as exception:
+            return {"message": str(exception)}, 400
 
     def delete_record(self, Question_ID):
         try:
@@ -155,4 +220,3 @@ class MyDatabase():
 if __name__ == '__main__':
     db = MyDatabase()
     db.create_tables()
-    # db.drop_tables()
