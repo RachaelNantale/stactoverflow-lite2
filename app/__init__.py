ba32cdce7a0326eeaef1a94026910a3e7ec7c40f
RachaelNantale <rachaelnantale42@gmail.com>
2018-08-30 07:32:21 +0300

    [Chore]Remove unwanted imports

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from app.views.views import bp
 from app.views.answer_views import answer_bp
 from app.views.user_views import user_bp
-from flask_bcrypt import Bcrypt
+
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token, get_jwt_identity)
 
@@ -12,7 +12,6 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config['JWT_SECRET_KEY'] = 'sup3rsecr3t'
     jwt = JWTManager(app)
-    bcrypt = Bcrypt(app)
 
     app.config.from_object(config_name)
     app.register_blueprint(bp)
